52fac1d0b8ae4eb3678246081e80cf618b2e034a
Suman Vsetty <suman.vummadi@gmail.com>
2026-09-24 19:13:49 +0530

    New Apeiro app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ android {
 
             signingConfig = signingConfigs.getByName("release")
 
-            resValue("string", "app_name", "Daystream")
+            resValue("string", "app_name", "Apeiro")
         }
 
         debug {
@@ -70,7 +70,7 @@ android {
             signingConfig = signingConfigs.getByName("debug")
 
             applicationIdSuffix = ".debug"
-            resValue("string", "app_name", "Daystream Debug")
+            resValue("string", "app_name", "Apeiro Debug")
         }
 
         create("demo") {
@@ -89,7 +89,7 @@ android {
             signingConfig = signingConfigs.getByName("debug")
 
             applicationIdSuffix = ".debug"
-            resValue("string", "app_name", "Daystream")
+            resValue("string", "app_name", "Apeiro")
         }
     }
 
